67371b8677a0ae290da8b6aee321e83c334f0723
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2018-09-26 02:18:57 -0300

    initial application

---
+++ alurapic/src/components/shared/imagem-responsiva/ImagemResponsiva.vue
@@ -0,0 +1,16 @@
+<template>
+  <img class="imagem-responsiva" :src="url" :alt="titulo">
+  <!-- diretiva v-bind ou seu atalho :bind para interpolação dentro de um atributo -->
+</template>
+
+<script>
+export default {
+  props: ["url", "titulo"]
+};
+</script>
+
+<style scoped>
+.imagem-responsiva {
+  width: 100%;
+}
+</style>
